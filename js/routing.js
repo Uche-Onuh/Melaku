bae10484c04f95a410d57fd4fd174d78a60ffdfb
Onuh Uche <onuhblaze@gmail.com>
2022-08-24 09:10:59 +0100

    First Commit

---
+++ js/routing.js
@@ -0,0 +1,33 @@
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/contact", function (req, res) {
+  res.sendFile(__dirname + "/contact.html");
+});
+app.get("/booking", function (req, res) {
+  res.sendFile(__dirname + "/booking.html");
+});
+app.get("/gallery", function (req, res) {
+  res.sendFile(__dirname + "gallery.html");
+});
+app.get("/page2", function (req, res) {
+  res.sendFile(__dirname + "gallery-page2.html");
+});
+app.get("/page3", function (req, res) {
+  res.sendFile(__dirname + "gallery-page3.html");
+});
+app.get("/page4", function (req, res) {
+  res.sendFile(__dirname + "gallery-page4.html");
+});
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "about.html");
+});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
